db09a3a5116e68453f934cd3eebf21f2d14b3079
Kody Liou <kody.liou@gmail.com>
2024-08-06 17:03:52 +0800

    Remove a lot of redundant things

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,9 @@
-import eslintConfigPrettier from "eslint-config-prettier";
+import eslintPluginReact from "eslint-plugin-react";
+import eslintPluginReactHooks from "eslint-plugin-react-hooks";
 
 export default [
-  eslintConfigPrettier,
+  eslintPluginReact,
+  eslintPluginReactHooks,
   {
     ignores: [
       '/dist/', '*.mjs',
@@ -20,6 +22,24 @@ export default [
       '/junit',
     ],
   },
+  // TODO: Make website folder use the rule which is for React.js jsx
+  {
+    files: ["website/**/*.jsx", "website/**/*.js"],
+    rules: {
+      "react/jsx-uses-react": "error",
+      "react/jsx-uses-vars": "error",
+      "react/react-in-jsx-scope": "error",
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn",
+    },
+    plugins: {
+      react: eslintPluginReact,
+      reactHooks: eslintPluginReactHooks,
+    },
+    settings: {
+      react: {
+        version: "detect", // Automatically detect the React version
+      },
+    },
+  },
 ];
-
-// TODO: Make website folder use the rule which is for React.js jsx
