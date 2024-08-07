d8e6bc5e52084bec39f1689fbaea90108f66e962
Kody Liou <kody.liou@gmail.com>
2024-08-07 16:08:05 +0800

    Ignore website

--- eslint.config.js
+++ eslint.config.js
@@ -1,45 +1,7 @@
-import eslintPluginReact from "eslint-plugin-react";
-import eslintPluginReactHooks from "eslint-plugin-react-hooks";
-
 export default [
-  eslintPluginReact,
-  eslintPluginReactHooks,
-  {
-    ignores: [
-      '/dist/', '*.mjs',
-      '/__tests__/__fixtures__',
-      '/docs',
-      '/node_modules',
-      '/website',
-      // Build artifacts
-      '/__tests__/__examples__',
-      '/coverage',
-      '/http',
-      '/index.js',
-      '/internal-apis.js',
-      '/index.umd.min.js',
-      '/internal-apis.umd.min.js',
-      '/junit',
-    ],
-  },
-  // TODO: Make website folder use the rule which is for React.js jsx
-  {
-    files: ["website/**/*.jsx", "website/**/*.js"],
-    rules: {
-      "react/jsx-uses-react": "error",
-      "react/jsx-uses-vars": "error",
-      "react/react-in-jsx-scope": "error",
-      "react-hooks/rules-of-hooks": "error",
-      "react-hooks/exhaustive-deps": "warn",
+    {
+        ignores: [
+            'website/**/*',
+        ],
     },
-    plugins: {
-      react: eslintPluginReact,
-      reactHooks: eslintPluginReactHooks,
-    },
-    settings: {
-      react: {
-        version: "detect", // Automatically detect the React version
-      },
-    },
-  },
 ];
